216b04384f1d79905277c6e52961de9906fe483f
Makar Baderko <makarbaderko@gmail.com>
2020-06-11 12:32:31 +0300

    COmpleted login system

--- test.cpp
+++ test.cpp
@@ -1,13 +1,13 @@
 #include "Directions.h"
 #include <conio.h>
 
-void printVector(vector <string> v) {
+void printVector(<vector> v) {
     for (int i = 0; i < v.size(); i++) 
         cout << v.at(i) << endl;
 }
 
-int login(){
-    cout << "1)Admin 2)User"
+int login(Direction &dir){
+    cout << "1)Admin 2)User";
     int ans;
     cin >> ans;
     switch(ans){
@@ -19,13 +19,45 @@ int login(){
                 return 1;
             } else {
                 cout << "Let's continue";
-            }
+                cout << "What's the name of the direction you want to add?";
+                string dir_name;
+                cin >> dir_name;
+                cout << "What category do you want to put it in?";
+                cout << "1)FirstStepsDirs;\n2)PaintAnimateDirs;\n3)VideoEditDirs;\n4)ModelingDirs;\n5)WebProgrammDirs;\n6)MobileProgrammDirs;\n7)HardProgrammDirs;\n8)GameProgrammDirs;\n";
+                cin >> ans;
+                switch(ans){
+                    case 1:
+                        dir.FirstStepsDirs.push_back(dir_name);
+                        break;
+                    case 2:
+                        dir.PaintAnimateDirs.push_back(dir_name);
+                        break;
+                    case 3:
+                        dir.VideoEditDirs.push_back(dir_name);
+                        break;
+                    case 4:
+                        dir.ModelingDirs.push_back(dir_name);
+                        break;
+                    case 5:
+                        dir.HardProgrammDirs.push_back(dir_name);
+                        break;
+                    case 6:
+                        dir.MobileProgrammDirs.push_back(dir_name);
+                        break;
+                    case 7:
+                        dir.HardProgrammDirs.push_back(dir_name);
+                        break;
+                    case 8:
+                        dir.GameProgrammDirs.push_back(dir_name);
+                        break;
+                }
         case 2:
             return 2
     }
 
     }
 }
+
 int main()
 {
     SetConsoleOutputCP(1251);
